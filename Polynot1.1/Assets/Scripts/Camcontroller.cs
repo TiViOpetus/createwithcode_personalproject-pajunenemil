9954a99379a52d6850735453ee7fdcff29aee201
pajunenemil <97280425+pajunenemil@users.noreply.github.com>
2022-02-17 13:51:08 +0200

    Kameroiden vaihto koodi lisätty

---
+++ Polynot1.1/Assets/Scripts/Camcontroller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camcontroller : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Switch1"))
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+        }
+        if (Input.GetButtonDown("Switch2"))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+            cam3.SetActive(false);
+        }
+        if (Input.GetButtonDown("Switch3"))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
+        }
+    }
+}
