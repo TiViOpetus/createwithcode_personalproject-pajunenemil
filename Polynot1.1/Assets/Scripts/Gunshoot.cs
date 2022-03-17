1647366e4ab73ba76d8e11d6b17a0f496eab176c
Emil <97280425+pajunenemil@users.noreply.github.com>
2022-03-17 08:50:35 +0200

    ase toimii ja ampuu

---
+++ Polynot1.1/Assets/Scripts/Gunshoot.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Gunshoot : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float speed = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+        }
+    }
+
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
