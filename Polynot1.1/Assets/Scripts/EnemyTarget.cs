3121f6f17ea033a614c912f8cf145749b909d4dd
pajunenemil <42013@edu.raseko.fi>
2022-05-02 17:01:41 +0300

    vihollinen ottaa osumaa nyt

---
+++ Polynot1.1/Assets/Scripts/EnemyTarget.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTarget : MonoBehaviour
+{
+    public float health = 100f;
+
+    public GameObject deadBody;
+
+    public bool created = false;
+
+    public void ApplyDamage(float amount)
+    {
+        health -= Mathf.Abs(amount);
+
+        if (health <= 0)
+        {
+            if (!created)
+            {
+                Instantiate(deadBody, transform.position, transform.localRotation);
+                created = true;
+            }
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
