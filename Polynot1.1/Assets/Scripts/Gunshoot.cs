37c05f1a3887a6f9539e0a4afcd6068e6e030fd0
pajunenemil <42013@edu.raseko.fi>
2022-05-04 18:15:37 +0300

    uusi koodi tehty ja peliä viimeistelty

--- Polynot1.1/Assets/Scripts/Gunshoot.cs
+++ Polynot1.1/Assets/Scripts/Gunshoot.cs
@@ -1,21 +1,25 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Gunshoot : MonoBehaviour
 {
-    public Rigidbody projectile;
-    public float speed = 20;
+    public GameObject Bullet;
 
-    // Update is called once per frame
+    public float Force = 2000f;
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
-            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+            GameObject BulletHolder;
+            BulletHolder = Instantiate(Bullet, transform.position, transform.rotation) as GameObject;
+            BulletHolder.transform.Rotate(Vector3.left * 90);
+
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = BulletHolder.GetComponent<Rigidbody>();
+            Temporary_RigidBody.AddForce(transform.forward * Force);
+            Destroy(BulletHolder, 2.0f);
         }
     }
-
-
-
 }
